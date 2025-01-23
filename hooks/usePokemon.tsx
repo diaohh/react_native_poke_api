da45a5d910007fcf301c6b3cd1b1c66f30644bad
Diego Vinasco <diegovitru2000@gmail.com>
2025-01-23 01:50:45 -0500

    add usePokemon hook and service

--- hooks/usePokemon.tsx
+++ hooks/usePokemon.tsx
@@ -8,21 +8,34 @@ export const usePokemon = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
-    getPokemons().then((res) => {
-      setPokemons(res.data.results)
-      setNextUrl(res.data.next)
-    }).finally(() => setLoading(false))
-  }, [])
-
+    setLoading(true);
+    getPokemons()
+      .then((res) => {
+        setPokemons(res.data.results);
+        setNextUrl(res.data.next);
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
   const getNextPokemons = () => {
-    setLoading(true)
-    getPokemons(nextUrl).then((res) => {
-      setPokemons(pokemons.concat(res.data.results))
-      setNextUrl(res.data.next)
-    }).finally(() => setLoading(false))
-  }
+    setLoading(true);
+    getPokemons(nextUrl)
+      .then((res) => {
+        setPokemons(pokemons.concat(res.data.results));
+        setNextUrl(res.data.next);
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const addFavorite = (pokemonData: {name: string, url: string}) => {
+    setFavorites([...favorites, pokemonData]);
+    setPokemons(pokemons.filter((e) => e.name !== pokemonData.name));
+  };
+
+  const removeFavorite = (pokemonData: {name: string, url: string}) => {
+    setPokemons([...pokemons, pokemonData]);
+    setFavorites(favorites.filter((e) => e.name !== pokemonData.name));
+  };
 
   return {
     pokemons,
@@ -30,6 +43,8 @@ export const usePokemon = () => {
     favorites,
     setFavorites,
     loading,
-    getNextPokemons
-  }
-}
+    getNextPokemons,
+    addFavorite,
+    removeFavorite,
+  };
+};
--- services/pokemonSvc.ts
+++ services/pokemonSvc.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export const getPokemons = async (url = "https://pokeapi.co/api/v2/pokemon"): Promise<any> => {
-  return await axios.get(url);
+export const getPokemons = async (url?: string): Promise<any> => {
+  return await axios.get(url || "https://pokeapi.co/api/v2/pokemon");
 }
